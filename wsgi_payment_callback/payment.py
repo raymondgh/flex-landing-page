8d696f3bfa99c5713866dcf34f6a069457ccfc6e
etisab <ariasabeti@gmail.com>
2015-12-25 00:21:16 -0800

    logging prefs

--- wsgi_payment_callback/payment.py
+++ wsgi_payment_callback/payment.py
@@ -7,6 +7,12 @@ import config
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
+trans_log = logging.getLogger('trans_logger')
+
+trans_handler = logging.FileHandler('trans.log')
+trans_handler.setLevel(logging.INFO)
+trans_log.addHandler(trans_handler)
+
 stripe.api_key = config.StripeConfig.TEST_STRIPE_SECRET_KEY
 
 app = Flask(__name__)
@@ -15,6 +21,8 @@ app = Flask(__name__)
 def charge():
 
     token = request.form['stripeToken']
+    purchase_pref = request.form['purchasePref']
+
     try:
         charge = stripe.Charge.create(
             amount=2900,
@@ -26,6 +34,9 @@ def charge():
     except stripe.error.CardError, e:
         # The card has been declined
         pass
+
+    trans_log.info(str(token) + ", " + str(purchase_pref))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
