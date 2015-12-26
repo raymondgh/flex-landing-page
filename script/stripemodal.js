20cf7649253109970559da6f42ac794ed0689ac7
etisab <ariasabeti@gmail.com>
2015-12-25 17:04:47 -0800

    extract stripe handler

---
+++ script/stripemodal.js
@@ -0,0 +1,23 @@
+var handler = StripeCheckout.configure({
+  key: 'pk_live_zOn9v7b2BQII3NYxpCENGl4W',
+  image: '/images/logo_spaced.png',
+  locale: 'auto',
+  token: function(token) {
+    $.post('payment/', {stripeToken: token.id, purchasePref: $("#pdf-checkbox").prop('checked')})
+  }
+});
+
+$('#customButton').on('click', function(e) {
+  // Open Checkout with further options
+  handler.open({
+    name: 'Leasetogether.com',
+    description: 'Leasetogether Preorder',
+    amount: 2900
+  });
+  e.preventDefault();
+});
+
+// Close Checkout on page navigation
+$(window).on('popstate', function() {
+  handler.close();
+});
